34c4a16949e1824065f74ec1e572c2d37e57a487
yongil-honam <yikim0711@gmail.com>
2020-07-21 14:28:04 +0900

    exam04

---
+++ ThreadTest/src/exam_04_1/User2.java
@@ -0,0 +1,17 @@
+package exam_04_1;
+
+public class User2 extends Thread {
+
+	private Calculator calculator;
+	
+	public void setCalculator(Calculator calculator) {
+		this.setName("User2");
+		this.calculator = calculator;
+	}
+	
+	@Override
+	public void run() {
+		calculator.setMemory(50);
+	}
+}
+	
